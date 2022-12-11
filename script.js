1d5227492bad6e8c88e0986f61c69162035ce932
IlieChrisitan <cristi_ilie99@yahoo.com>
2022-12-12 01:42:33 +0200

    .

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const clear = document.querySelector('.clear');
 const comma = document.querySelector('.comma');
 const signs = document.querySelectorAll('.sign');
 const equal = document.querySelector('.equal');
+const negative = document.querySelector('.negative');
 let resultValue = "";
 let sign;
+let keycode
 let numberValue  = "";
 let sw = false;
 
@@ -13,11 +15,53 @@ for(let i = 0 ; i < signs.length ; i++){
     signs[i].disabled = false;
 }
 
+// document.addEventListener('keypress', (e) => {
+//     keycode = e.key;
+//     if(keycode >= 0 && keycode <= 9) getNumber(keycode);
+    
+//     switch (keycode){
+//         case "+":
+//             resultValue += numberValue + keycode;
+//             numberValue = "";
+//             result.textContent = "";
+//         case "-":
+//             resultValue += numberValue + keycode;
+//             numberValue = "";
+//         case "*":
+//             resultValue += numberValue + keycode;
+//             numberValue = "";
+//         case "/":
+//             resultValue += numberValue + keycode;
+//             numberValue = "";
+//         case "Enter":
+//             total();
+//     }
+    
+// }, false);
+
+// Add event listener on keypress
+// document.addEventListener('keypress', (event) => {
+//     var name = event.key;
+//     var code = event.code;
+//     // Alert the key name and key code on keydown
+//     alert(`Key pressed ${name} \r\n Key code value: ${code}`);
+// }, false);
+
 clear.addEventListener('click', () => {
     result.textContent = "";
     comma.disabled = false;
     numberValue = "";
     resultValue = "";
+});
+
+negative.addEventListener('click', () => {
+    if(numberValue.charAt(0) !== "-" && numberValue !== ""){
+        numberValue = "-".concat(numberValue);
+        result.textContent = numberValue;
+    } else if(numberValue.charAt(0) === "-"){
+        numberValue = numberValue.slice(1)
+        result.textContent = numberValue;
+    }
 });
 
 for(let i = 0 ; i < number.length ; i++) {
@@ -51,7 +95,6 @@ function getSign() {
                 }
                 signs[i].disabled = true;
             }
-            
             sw = true;
         });
     };
@@ -59,6 +102,9 @@ function getSign() {
 getSign();
  
 comma.addEventListener('click', () => {
+    if(numberValue === ""){
+        getNumber("0")
+    }
     getNumber(comma.getAttribute('value'));
     comma.disabled = true;
 });
@@ -68,7 +114,7 @@ function addSign() {
         for(let i = 0; i < signs.length ; i++){
             signs[i].disabled = false;
         }
-        resultValue += numberValue + sign
+        resultValue += numberValue + sign;
         result.textContent = "";
         numberValue = "";
         comma.disabled = false;
@@ -83,14 +129,19 @@ function getNumber(atr) {
         numberValue += atr;        
     };  
     result.textContent = numberValue;
+    console.log(numberValue)
 };
 
-equal.addEventListener('click', () =>{
+function total() {
     resultValue += numberValue;
-    resultValue = resultValue.replace(/x/g, "*");
+    resultValue = resultValue.replace(/x/g, "*").replace(/,/g, ".").replace(/--/g, "+");
     result.textContent = eval(resultValue);
-    console.log(resultValue)
     resultValue = "";
     numberValue = "";
     comma.disabled = false;
+    console.log(resultValue)
+};
+
+equal.addEventListener('click', () => {
+    total();
 });
